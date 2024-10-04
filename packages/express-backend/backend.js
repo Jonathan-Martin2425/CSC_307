4413812373f15777bd5359b0179d6bee79a6f3b4
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2024-10-03 20:20:57 -0700

    IA 2 submission #1

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -28,7 +28,7 @@ const users = {
         {
             id: "zap555",
             name: "Dennis",
-                job: "Bartender"
+            job: "Bartender"
         }
     ]
 };
@@ -39,8 +39,30 @@ const findUserByName = (name) => {
     );
 };
 
+const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name && user["job"] === job
+    );
+};
+
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+const deleteUser = (userID) => {
+    for (let i = 0; i < users["users_list"].length; i = i + 1) {
+        if (users["users_list"][i]["id"] === userID){
+            users["users_list"].splice(i, 1);   
+        }
+    }
+    return;
+};
+
+app.use(express.json());
 
 app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
@@ -52,8 +74,6 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -61,15 +81,39 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
     const name = req.query.name;
-    if (name != undefined) {
+    const job = req.query.job;
+
+    //gives the list of users that match name AND job
+    //if there's no or incorrect querry, returns whole user list instead
+    if (name != undefined && job != undefined) {
+        let result = findUserByNameAndJob(name, job);
+        console.log(name + " " + job);
+        result = { users_list: result };
+        res.send(result);
+    }
+    else if (name != undefined) {
         let result = findUserByName(name);
         result = { users_list: result };
-        res.send(result);s
+        res.send(result);
     } else {
         res.send(users);
     }
 });
 
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
+app.delete("/users", (req, res) => {
+    const userId = req.query.id;
+    console.log(userId);
+    if (userId != undefined) {
+        deleteUser(userId);
+    }
+    res.send();
+});
 
 app.listen(port, () => {
   console.log(
